d135646ae2d76f94ea856f8c93ee2d482c3ce797
Al Lukman <allukman1802@gmail.com>
2022-10-04 14:32:03 +0700

    add cart

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Fragment, useState } from "react";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+
+function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  const showCartHandler = () => {
+    setShowCart(true);
+  };
+
+  const hideCartHandler = () => {
+    setShowCart(false);
+  };
+
+  return (
+    <Fragment>
+      {showCart && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
